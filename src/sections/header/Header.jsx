c898cf5af570fafb2024fe54892611f0acd17313
duong\DUONG <duomg0023@gmail.com>
2024-10-29 13:09:21 +0700

    header

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,27 @@
+import portaint from '../../assets/portaint.jpg'
 import './header.css'
+import data from './data.js'
 
 const Header = () => {
     return (
-        <section id="header"><h2>Header</h2></section>
+        <header id="header">
+            <div className="container header__container">
+                <div className="header_portfolio">
+                    <img src={portaint} alt="Header Portaint" />
+                </div>
+                <h3>Tô Đông Dương </h3>
+                <p>You are a click away from building your dream website or web app. Send me the details of your project for a modern, mobile responsive, highly performant website today!</p>
+                <div className="header__contact">
+                    <a href="#contact" className='btn primary'>Let's Talk</a>
+                    <a href="#portfolio" className='btn primary'>My Work</a>
+                </div>
+                <div className="header__socials">
+                    {
+                        data.map(item => <a key={item.id} href={item.link} target='_blank' rel='noopener noreferrer'>{item.icon}</a>)
+                    }
+                </div>
+            </div>
+        </header>
     )
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,11 @@
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+const data = [
+    { id: 1, link: 'https://www.facebook.com/tdd.2412', icon: <FaFacebook /> },
+    { id: 2, link: 'https://www.instagram.com/tdd.2412/', icon: <FaInstagram /> },
+    { id: 1, link: 'https://github.com/d2inthenow', icon: <FaGithub /> }
+]
+
+export default data
