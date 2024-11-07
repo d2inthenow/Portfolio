d6b0d2adf9fc7cca7f8494864c7c291e1c40c2d9
duong\DUONG <duomg0023@gmail.com>
2024-11-07 13:07:42 +0700

    Sevices

---
+++ src/sections/services/data.js
@@ -0,0 +1,20 @@
+
+import { RiReactjsLine } from 'react-icons/ri'
+import { DiNodejs } from "react-icons/di";
+import { FaEarthAmericas } from "react-icons/fa6";
+
+const data = [
+    {
+        id: 1, icon: <RiReactjsLine />, title: 'Frontend Development', desc: "Your product will look good and will be accessible on all devices, including mobile phones, tablets, and desktop."
+    },
+    {
+        id: 2, icon: <DiNodejs />, title: 'Backend Development', desc: "The security of your business/product is taken seriously right from the start of the project. I will make sure your website/app is secure from attacks."
+    },
+    {
+        id: 3, icon: <FaEarthAmericas />, title: 'Full Stack Development', desc: "I will create a full stack web application that will be able to handle all the data and will be able to interact with the user."
+    },
+
+]
+
+
+export default data
