7f35759a33eaeade47630b64f98f80c00ccaac2b
duong\DUONG <duomg0023@gmail.com>
2024-11-13 16:37:51 +0700

    Contact

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,8 +1,23 @@
-import './contact.css'
-const Contact = () => {
-    return (
-        <section id="contact"><h2>Contact</h2></section>
-    )
-}
+import contacts from "./data";
+import "./contact.css";
 
-export default Contact
+const Contact = () => {
+  return (
+    <section id="contact">
+      <div className="container contact__container">
+        {contacts.map((contact) => (
+          <a
+            key={contact.id}
+            href={contact.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {contact.icon}
+          </a>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/sections/contact/data.js
@@ -0,0 +1,11 @@
+import { FaInstagram } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+
+const data = [
+  { icon: <FaInstagram />, link: "https://www.instagram.com/tdd.2412" },
+  { icon: <FaFacebook />, link: "https://www.facebook.com/tdd.2412" },
+  { icon: <FaPhone />, link: "https://zalo.me/0368205272" },
+];
+
+export default data;
